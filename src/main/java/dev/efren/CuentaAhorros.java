44eb1aa906bfc5dc751c8a286ae44b3b0bd9cc18
EfrenTC <efrentomascampa@gmail.com>
2025-08-14 15:02:46 +0200

    feature: clase hija ahorros

---
+++ src/main/java/dev/efren/CuentaAhorros.java
@@ -0,0 +1,44 @@
+package dev.efren;
+
+public class CuentaAhorros extends Cuenta {
+    private boolean activa;
+
+    public CuentaAhorros(float saldo, float tasaAnual) {
+        super(saldo, tasaAnual);
+        this.activa = saldo >= 10000;
+    }
+
+    @Override
+    public void consignar(float cantidad) {
+        if (activa) {   
+            super.consignar(cantidad);
+        }
+    }
+
+    @Override
+    public void retirar(float cantidad) {
+        if (activa) {
+            super.retirar(cantidad);
+        }
+    }
+
+    @Override
+    public void extractoMensual() {
+        if (numeroRetiros > 4) {
+            comisionMensual += (numeroRetiros - 4) * 1000;
+        }
+        super.extractoMensual();
+        activa = saldo >= 10000;
+    }
+
+    @Override
+    public String imprimir() {
+        return "Saldo: " + saldo +
+               ", Comisión mensual: " + comisionMensual +
+               ", Transacciones: " + (numeroConsignaciones + numeroRetiros);
+    }
+
+    public boolean isActiva() {
+        return activa;
+    }
+}
