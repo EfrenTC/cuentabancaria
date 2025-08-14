271616021a83be34f5efead843cf58b759849c98
EfrenTC <efrentomascampa@gmail.com>
2025-08-14 15:03:55 +0200

    feature: main java

--- src/main/java/dev/efren/App.java
+++ src/main/java/dev/efren/App.java
@@ -1,17 +1,17 @@
 package dev.efren;
 
-/**
- * Hello world!
- */
-public final class App {
-    private App() {
-    }
-
-    /**
-     * Says hello to the world.
-     * @param args The arguments of the program.
-     */
+public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        CuentaAhorros ca = new CuentaAhorros(12000, 12);
+        ca.retirar(2000);
+        ca.consignar(5000);
+        ca.extractoMensual();
+        System.out.println(ca.imprimir());
+
+        CuentaCorriente cc = new CuentaCorriente(5000, 12);
+        cc.retirar(8000);
+        cc.consignar(2000);
+        cc.extractoMensual();
+        System.out.println(cc.imprimir());
     }
 }
